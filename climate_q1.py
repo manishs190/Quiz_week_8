b84cdbaf8a3345e2d8d51d1b1d88b26d20e7983a
Manish Shrestha <manish.shrestha@griffithuni.edu.au>
2023-09-14 13:43:42 +1000

    Made db connecting in Sqlite for climate_q1.py

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,19 @@
+import sqlite3
 import matplotlib.pyplot as plt
-        
+
 years = []
 co2 = []
 temp = []
+
+connection = sqlite3.connect("climate.db")
+cursor = connection.cursor()
+sql_cmd1 = """SELECT * FROM ClimateData;"""
+cursor.execute(sql_cmd1)
+result = cursor.fetchall()
+for r in result:
+    years.append(r[0])
+    co2.append(r[1])
+    temp.append(r[2])
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -15,4 +26,5 @@ plt.plot(years, temp, 'r*-')
 plt.ylabel("Temp (C)") 
 plt.xlabel("Year (decade)") 
 plt.show() 
-plt.savefig("co2_temp_1.png") 
+plt.savefig("co2_temp_1.png")
+connection.close()
